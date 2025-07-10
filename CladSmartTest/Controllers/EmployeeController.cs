b3cf719787b0087a3d0b9eac494fa21bd5e725bf
hendrakurniawan1998.work <hendrakurniawan1998.work@gmail.com>
2025-07-10 20:50:06 +0700

    Add position selection and improve edit form layout
    Updated `EmployeeController.cs` to include a dropdown list for employee positions in the edit view. Modified `Edit.cshtml` to change the title to "Employee Detail" and enhance the form layout with clear labels and better organization for input fields, including dropdowns for Department and Position. Improved alignment and grouping of fields, and repositioned the submit button for better usability.

--- CladSmartTest/Controllers/EmployeeController.cs
+++ CladSmartTest/Controllers/EmployeeController.cs
@@ -73,6 +73,18 @@ namespace CladSmartTest.Controllers
             {
                 return HttpNotFound();
             }
+
+            List<SelectListItem> positions = new List<SelectListItem>()
+            {
+                new SelectListItem(){ Text = "Operator", Value= "Operator"},
+                new SelectListItem(){ Text = "Technician", Value= "Technician"},
+                new SelectListItem(){ Text = "Leader", Value= "Leader"},
+                new SelectListItem(){ Text = "Supervisor", Value= "Supervisor"},
+                new SelectListItem(){ Text = "Manager", Value= "Manager"},
+            };
+
+            ViewBag.position = new SelectList(positions, "Value", "Text", employee.position);
+
             ViewBag.department_id = new SelectList(db.departments, "id", "department_name", employee.department_id);
             return View(employee);
         }
