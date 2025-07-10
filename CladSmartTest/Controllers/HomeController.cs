acdb9b7f04e46fe0228f305b45fedf1c37339be1
hendrakurniawan1998.work <hendrakurniawan1998.work@gmail.com>
2025-07-10 13:54:17 +0700

    Add Entity Framework support and restructure data layer
    Add Entity Framework support and employee management
    
    Updated project to include Entity Framework 6.2.0 and SQL Server provider. Added new data layer files generated from the .edmx model. Modified Web.config for database connection settings and updated packages.config. Enhanced HomeController to retrieve employee data, updated views for employee display, and removed the old Contact view. Generated entity classes for department and employee, and created the .edmx file for visual representation of the model.

--- CladSmartTest/Controllers/HomeController.cs
+++ CladSmartTest/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CladSmartTest.DataLayer;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -22,7 +24,52 @@ namespace CladSmartTest.Controllers
 
         public ActionResult Employee()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page. Test";
+
+            // Ensure cladsmartEntities implements IDisposable  
+            var db = new cladsmartEntities();
+
+            // Define a list to hold employee data
+            List<employee> employeeList = new List<employee>();
+
+
+            // Retrieve all employees and their departments  
+            var employees = db.employees
+                    .Select(e => new
+                    {
+                        e.nik,
+                        e.name,
+                        e.position,
+                        e.bpjs_allowance,
+                        e.meal_allowance,
+                        e.laptop_allowance,
+                        DepartmentName = e.department.department_name
+                    })
+                    .ToList();
+
+            // Convert anonymous types to a list of dictionaries for easier access in the view
+           
+           foreach (var employee in employees)
+            {
+                employee emp = new employee();
+                emp.nik = employee.nik;
+                emp.name = employee.name;
+                emp.position = employee.position;
+                emp.bpjs_allowance = employee.bpjs_allowance;  
+                emp.meal_allowance = employee.meal_allowance;
+                emp.laptop_allowance = employee.laptop_allowance;
+                emp.department = new department
+                {
+                    department_name = employee.DepartmentName
+                };
+
+                employeeList.Add(emp); 
+            }
+
+
+
+            // Pass the employee data to the view  
+            ViewBag.Employees = employeeList;
 
             return View();
         }
