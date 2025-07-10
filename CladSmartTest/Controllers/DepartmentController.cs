843cbc8b65c60adaa8928e4ddb6b93aeeb7e62e4
hendrakurniawan1998.work <hendrakurniawan1998.work@gmail.com>
2025-07-10 18:58:29 +0700

    Add department and employee management features
    Updated project structure to include new DepartmentController and EmployeeController with full CRUD operations. Created corresponding views for departments and employees, enhancing the application with improved data handling and presentation. Removed outdated views and updated the connection string in Web.config.

---
+++ CladSmartTest/Controllers/DepartmentController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CladSmartTest.DataLayer;
+
+namespace CladSmartTest.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private cladsmartEntities db = new cladsmartEntities();
+
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View(db.departments.ToList());
+        }
+
+        // GET: Department/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            department department = db.departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // GET: Department/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Department/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,department_name")] department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.departments.Add(department);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(department);
+        }
+
+        // GET: Department/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            department department = db.departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Department/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,department_name")] department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(department).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(department);
+        }
+
+        // GET: Department/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            department department = db.departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Department/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            department department = db.departments.Find(id);
+            db.departments.Remove(department);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CladSmartTest/Controllers/EmployeeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CladSmartTest.DataLayer;
+
+namespace CladSmartTest.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private cladsmartEntities db = new cladsmartEntities();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employees = db.employees.Include(e => e.department);
+            return View(employees.ToList());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = db.employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            ViewBag.department_id = new SelectList(db.departments, "id", "department_name");
+            return View();
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nik,name,department_id,position,bpjs_allowance,meal_allowance,laptop_allowance")] employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.department_id = new SelectList(db.departments, "id", "department_name", employee.department_id);
+            return View(employee);
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = db.employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.department_id = new SelectList(db.departments, "id", "department_name", employee.department_id);
+            return View(employee);
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "nik,name,department_id,position,bpjs_allowance,meal_allowance,laptop_allowance")] employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.department_id = new SelectList(db.departments, "id", "department_name", employee.department_id);
+            return View(employee);
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = db.employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            employee employee = db.employees.Find(id);
+            db.employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
